b86a6e846c6cc691a030ba507839b303b8eaea84
Raj Savita <raj.savita992@ethara.ai>
2026-05-17 02:38:49 +0530

    fix vite allowed hosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  
   server: {
     proxy: {
       "/api": {
@@ -12,10 +11,7 @@ export default defineConfig({
       }
     }
   },
-
   preview: {
-    allowedHosts: [
-      "team-task-manager-client-production-11a9.up.railway.app"
-    ]
+    allowedHosts: true
   }
 });
