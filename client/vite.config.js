d8dc3ad1d4189346626f8fb05ad328cc0f14e529
Raj Savita <raj.savita992@ethara.ai>
2026-05-17 02:22:56 +0530

    fix vite railway host

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     proxy: {
       "/api": {
@@ -10,5 +11,11 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+
+  preview: {
+    allowedHosts: [
+      "team-task-manager-client-production-11a9.up.railway.app"
+    ]
   }
 });
